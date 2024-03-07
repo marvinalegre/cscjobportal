483411a77b957b3401d840dbe78c5dcb00ee5621
Marvin Alegre <marvinalegre@skiff.com>
2024-03-07 22:24:10 +0800

    fix: add missing sleep func in probe script

--- api/scripts/probe.js
+++ api/scripts/probe.js
@@ -1,17 +1,18 @@
 import { exec, execSync } from "child_process";
-import sleep from "../lib/sleep.js";
 
 //////////////////////////////////////////////
 const REQ_PER_SECOND = 50;
 const START = 3730001;
-const END = 3739000;
-const GAP = 100;
+const END = 3744000;
+const GAP = 200;
 
 //////////////////////////////////////////////
 execSync("rm -rf posts; mkdir posts;");
 
 let count = 0;
 for (let i = START; i < END + 1; i += GAP) {
+  console.log(i)
+  
   exec(
     `wget -q https://csc.gov.ph/career/job/${i} -O ./posts/${i}`,
     (error, stdout, stderr) => {
@@ -30,6 +31,12 @@ for (let i = START; i < END + 1; i += GAP) {
   count++;
 
   if (count % REQ_PER_SECOND === 0) {
-    await sleep(1000);
+    await sleep(1);
   }
+}
+
+function sleep(seconds) {
+  return new Promise(resolve => {
+    setTimeout(resolve, seconds * 1000);
+  });
 }
